028beb7799a0319b6338b3683c638f8115f800c2
IvanSitnikov1 <iw.sitnikoff@yandex.ru>
2023-10-10 21:54:46 +0300

    Написаны модели для 'Задачи', проведены миграции

--- main/models.py
+++ main/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 
 
-class Task:
-    pass
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        (1, 'Высокий - 1'),
+        (2, 'Средний - 2'),
+        (3, 'Низкий - 3'),
+    ]
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField()
+    active = models.BooleanField(default=True)
+    comments = models.ForeignKey('Comment', on_delete=models.CASCADE, blank=True, null=True)
+    author = models.CharField(max_length=255)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES)
+    subtask = models.ForeignKey('SubTask', on_delete=models.CASCADE, blank=True, null=True)
 
 
-class SubTask:
-    pass
+class SubTask(models.Model):
+    content = models.TextField()
+    active = models.BooleanField(default=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
 
-class Comment:
-    pass
+class Comment(models.Model):
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
