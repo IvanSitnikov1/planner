d09ce722754e679540a946aec04946c06153af91
IvanSitnikov1 <iw.sitnikoff@yandex.ru>
2023-11-25 16:55:18 +0300

    docker compose подготовлен к запуску на сервере

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ DJANGO_SUPERUSER_USERNAME="admin" \
   DJANGO_SUPERUSER_EMAIL="admin@example.com" \
   python manage.py createsuperuser --noinput
 
-#python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 python manage.py runserver --noreload 0.0.0.0:8000
